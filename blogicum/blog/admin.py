ab185e31b6c90eccd990614612ad13a0122d9ad3
Nikita Sysenko <nikitagornostaev986@gmail.com>
2025-08-29 20:40:04 +0300

    Fix project structure: remove nested folder

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Category, Location, Comment, Post
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'slug')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date', 'is_published')
+    list_filter = ('is_published', 'category')
+    search_fields = ('title', 'text')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('text', 'author__username', 'post__title')
---
+++ blogicum/blog/models.py
@@ -0,0 +1,159 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+
+User = get_user_model()
+
+SLUG_MAX_LENGTH = 64
+NAME_MAX_LENGTH = 256
+TITLE_MAX_LENGTH = 256
+
+
+class PublishedModel(models.Model):
+    """Абстрактная базовая модель с общими полями для публикуемого контента.
+    Служит основой для других моделей, требующих функционал публикации
+    и отслеживания времени создания. Не создает отдельную таблицу в БД.
+    """
+
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию от пользователей.'
+    )
+    created_at = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Location(PublishedModel):
+    """Модель географического местоположения для привязки контента.
+    Позволяет ассоциировать публикации с конкретными местами.
+    """
+
+    name = models.CharField(
+        verbose_name='Название места',
+        max_length=NAME_MAX_LENGTH)
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+        ordering = ('name',)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(PublishedModel):
+    """Модель Категории"""
+
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=TITLE_MAX_LENGTH)
+    description = models.TextField(
+        verbose_name='Описание')
+    slug = models.SlugField(
+        max_length=SLUG_MAX_LENGTH,
+        unique=True,
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'допускаются только латинские буквы,'
+            'цифры, дефис и подчёркивание.')
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('title',)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class PostQuerySet(models.QuerySet):
+    """Возвращает опубликованные посты с опубликованными категориями"""
+
+    def published(self) -> models.QuerySet:
+        return self.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True
+        ).order_by('-pub_date')
+
+
+class Post(PublishedModel):
+    """Модель Публикации"""
+
+    title = models.CharField(
+        verbose_name='Заголовок публикации',
+        max_length=TITLE_MAX_LENGTH)
+    text = models.TextField(
+        verbose_name='Текст публикации')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text=(
+            'Установите будущую дату для отложенной публикации - '
+            'пост автоматически станет доступен в указанное время.')
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        blank=True)
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации')
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Местоположение')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Категория')
+
+    objects = PostQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date',)
+        default_related_name = 'posts'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    """Модель комментария пользователей к публикациям."""
+
+    text = models.TextField(
+        verbose_name='Текст комментария')
+    created_at = models.DateTimeField(
+        verbose_name='Дата создания',
+        auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор комментария',
+        related_name='comments'
+    )
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        verbose_name='Публикация',
+        related_name='comments'
+    )
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('created_at',)
+
+    def __str__(self) -> str:
+        return self.text[:20]
